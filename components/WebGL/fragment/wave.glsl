c42ed03d08870d6def9e59185a1cc1f6e2cad2f0
l.orsini <l.orsini@mazarinedigital.com>
2021-01-14 11:39:38 +0100

    wave base : finish

--- components/WebGL/fragment/wave.glsl
+++ components/WebGL/fragment/wave.glsl
@@ -2,10 +2,17 @@
 #extension GL_OES_standard_derivatives : enable
 #endif
 
+precision mediump float;
+
 varying vec2 vUv;
+varying float vWave;
 uniform sampler2D uTexture;
 
 void main() {
-  vec3 texture = texture2D(uTexture, vUv).rgb;
+  float wave = vWave * 0.25;
+  float r = texture2D(uTexture, vUv + wave).r;
+  float g = texture2D(uTexture, vUv).g;
+  float b = texture2D(uTexture, vUv + wave).b;
+  vec3 texture = vec3(r, g, b);
   gl_FragColor = vec4(texture, 1.);
 }
--- components/WebGL/vertex/wave.glsl
+++ components/WebGL/vertex/wave.glsl
@@ -1,6 +1,7 @@
 precision mediump float;
 
 varying vec2 vUv;
+varying float vWave;
 uniform float uTime;
 
 //
@@ -113,6 +114,7 @@ void main() {
   float noiseAmp = 0.15; 
   vec3 noisePos = vec3(pos.x * noiseFreq + uTime, pos.y, pos.z);
   pos.z += snoise(noisePos) * noiseAmp;
+  vWave = pos.z;
 
   gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.);
 }
